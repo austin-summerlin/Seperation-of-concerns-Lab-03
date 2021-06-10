0b1092c776efe1737c47a30b59594cb714d450ca
Austin Summerlin <summerlin.austin@gmail.com>
2021-06-09 23:54:17 -0700

    first attempt at writing routes

--- orders.js
+++ orders.js
@@ -1,15 +1,39 @@
-/* eslint-disable indent */
 const { Router } = require('express');
 const Order = require('../models/Order');
 
 // export default Router.......
 module.exports = Router() // app.post(....)
- .post('/api/v1/orders', async (req, res) => {
-  try {
-   const order = await Order.insert(req.params.quantity);
-   res.send(order);
-  } catch (err) {
-   res.status(500).send(err);
-  }
- });
+  .post('/api/v1/orders', async (req, res) => {
+    try {
+      const order = await Order.insert(req.params.quantity);
+      res.send(order);
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  })
+  .put('/api/v1/orders/:id', async (req, res) => {
+    try {
+      const order = await Order.insert(req.params.quantity);
+      res.send(order);
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  })
+  .get('/api/v1/orders/:id', async (req, res) => {
+    try {
+      const order = await Order.insert(req.params.quantity);
+      res.send(order);
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  })
+  .delete('/api/v1/orders/:id', async (req, res) => {
+    try {
+      const order = await Order.insert(req.params.quantity);
+      res.send(order);
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  });
+
 
